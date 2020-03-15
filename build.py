ce4b51ac8cbcf7c78406c884760e4e3c269fe062
Jeff Gilbert <jdashg@gmail.com>
2020-03-14 18:44:10 -0700

    Move manifest.json to web-ext/. to support Chrome.

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import pathlib
 import shutil
 
 DIR = pathlib.Path(__file__).parent
-OUT = DIR / 'out'
+WEB_EXT_OUT = DIR / 'web-ext/out'
 
 def write_to(dest, data):
   print(f'   ({len(data)} bytes) => {dest}')
@@ -17,17 +17,14 @@ def write_to(dest, data):
 def clean():
   print('[clean]')
 
-  if OUT.exists():
-    shutil.rmtree(OUT)
+  if WEB_EXT_OUT.exists():
+    shutil.rmtree(WEB_EXT_OUT)
 
-  os.mkdir(OUT)
+  os.mkdir(WEB_EXT_OUT)
 
 
-def build_content_script():
-  print('[build_content_script]')
-
-  src = DIR / 'webgl-1on2.js'
-  dest = OUT / 'webgl-1on2.content.js'
+def build_content_script(src, dest):
+  print(f'[build_content_script {src}]')
 
   data = src.read_bytes()
   data = content_script_embed.from_script(data)
@@ -36,5 +33,5 @@ def build_content_script():
 # -
 
 clean();
-build_content_script()
+build_content_script(DIR / 'webgl-1on2.js', WEB_EXT_OUT / 'webgl-1on2.content.js')
 print('Build complete.')
